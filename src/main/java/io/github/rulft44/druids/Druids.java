05eaeb4ddf43b24221a882704b20fa49a105174f
Rulft44 <dimucciorubengermania@gmail.com>
2025-09-05 00:44:38 +0100

    :heavy_minus_sign: Removed More RPG Classes

--- src/main/java/io/github/rulft44/druids/Druids.java
+++ src/main/java/io/github/rulft44/druids/Druids.java
@@ -2,12 +2,17 @@ package io.github.rulft44.druids;
 
 import io.github.rulft44.druids.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.spell_power.api.SpellSchool;
+import net.spell_power.api.SpellSchools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Druids implements ModInitializer {
 	public static final String ID = "druids";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+	// Creates school with the id of `spell_power:blood`, default namespace: `spell_power`
+	public static final SpellSchool NATURE = SpellSchools.createMagic("nature", 0x43bf4b);
 
 	@Override
 	public void onInitialize() {
