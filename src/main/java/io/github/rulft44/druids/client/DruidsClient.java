086352afdbaae1564252e31f04aa8d5c33262ef9
Rulft44 <dimucciorubengermania@gmail.com>
2025-09-22 18:28:43 +0100

    :coffin: Remove Leaf Particle

--- src/main/java/io/github/rulft44/druids/client/DruidsClient.java
+++ src/main/java/io/github/rulft44/druids/client/DruidsClient.java
@@ -3,12 +3,9 @@ package io.github.rulft44.druids.client;
 import io.github.rulft44.druids.Druids;
 import io.github.rulft44.druids.client.armor.DruidArmorRenderer;
 import io.github.rulft44.druids.item.ModArmors;
-import io.github.rulft44.druids.particle.LeafParticle;
-import io.github.rulft44.druids.particle.ModParticles;
 import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRenderer;
 import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRendererRegistry;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.minecraft.util.Identifier;
 import net.spell_engine.api.item.armor.Armor;
 import net.spell_engine.api.render.CustomModels;
@@ -23,10 +20,7 @@ public class DruidsClient implements ClientModInitializer {
 			Identifier.of(Druids.ID, "projectile/bramble_shot")
 		));
 
-		ParticleFactoryRegistry.getInstance().register(ModParticles.LEAF, LeafParticle.Factory::new);
-
 		registerArmorRenderer(ModArmors.druidArmorSet_T1, DruidArmorRenderer::druid);
-
 	}
 
 	private static void registerArmorRenderer(Armor.Set set, Supplier<AzArmorRenderer> armorRendererSupplier) {
